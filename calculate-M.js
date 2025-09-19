6903e6e4ef1983242b3a4a48906c4d24458f9f84
yogurt0617 <20070617TimmyAlan@gmail.com>
2025-09-19 21:48:48 +0800

    新增小數與平方計算

--- calculate-M.js
+++ calculate-M.js
@@ -1,6 +1,10 @@
 
 let inputans = document.getElementById("inputans")
 
+function squrt(numsqurt){
+    return numsqurt * numsqurt
+}
+
 document.querySelectorAll("button").forEach(btn=>{
     btn.addEventListener("click",()=>{
         const value = btn.dataset.val
@@ -10,15 +14,18 @@ document.querySelectorAll("button").forEach(btn=>{
         if(value === "="){
             try{
                 inputans.value = eval(inputans.value)
+                console.log(inputans.value)
             }catch{
                 inputans.value = "錯誤"
             }
         }
-
+        else if(value === "squrt"){
+            var squrtx = squrt(inputans.value)
+            inputans.value = squrtx
+        }
         else if(value === "AC"){
             inputans.value =""
         }
-        
         else {
         inputans.value += value;
         }
