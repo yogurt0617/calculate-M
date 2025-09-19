03e3e3d5bf8b3115b8fdf2a40eb7993500834ef8
yogurt0617 <20070617TimmyAlan@gmail.com>
2025-09-20 00:14:37 +0800

    1.1版本更新，現在可以使用小數、平方次方

--- calculate-M.js
+++ calculate-M.js
@@ -1,34 +1,62 @@
 
 let inputans = document.getElementById("inputans")
-
+let nothing = document.getElementById("nothing")
+let nothingno = 0
+let inputing = 0
+let inputed = 0
+let op = "none"
 function squrt(numsqurt){
     return numsqurt * numsqurt
+}
+function power(numpowored,numpower){
+    return numpowored ** numpower
 }
 
 document.querySelectorAll("button").forEach(btn=>{
     btn.addEventListener("click",()=>{
         const value = btn.dataset.val
         console.log(btn.dataset.val)
-
         
         if(value === "="){
-            try{
-                inputans.value = eval(inputans.value)
-                console.log(inputans.value)
-            }catch{
-                inputans.value = "錯誤"
+            console.log(inputed)
+
+            if(op === "power"){
+                var powerx = power(Number(inputed),Number(inputans.value))
+                inputans.value = powerx
+                op = "none"
             }
+            
+            else {
+                try{
+                    inputans.value = eval(inputans.value)
+
+                }catch{
+                    inputans.value = "錯誤"
+            }
+        }
         }
         else if(value === "squrt"){
             var squrtx = squrt(inputans.value)
             inputans.value = squrtx
         }
+        else if(value === "power"){
+            inputed = inputans.value
+            inputans.value = ""
+            op = "power"
+        }
         else if(value === "AC"){
             inputans.value =""
         }
         else {
-        inputans.value += value;
+            inputans.value += value
         }
-
     })
+})
+
+nothing.addEventListener("click",()=>{
+    nothingno += 1
+    console.log(nothingno)
+    if(nothingno == 5){
+    inputans.value = "就還沒開發是在按什麼"
+    }
 })
