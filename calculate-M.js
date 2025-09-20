4701e502cf17933e2192a7dd8d62429f9cb59b7f
yogurt0617 <20070617TimmyAlan@gmail.com>
2025-09-20 12:52:35 +0800

    ver.1.2版本更新:平方次方運算邏輯更改，新增backspce鍵

--- calculate-M.js
+++ calculate-M.js
@@ -1,17 +1,18 @@
 
 let inputans = document.getElementById("inputans")
 let nothing = document.getElementById("nothing")
+let inputarry = []
 let nothingno = 0
 let inputing = 0
 let inputed = 0
 let op = "none"
+
 function squrt(numsqurt){
     return numsqurt * numsqurt
 }
 function power(numpowored,numpower){
     return numpowored ** numpower
 }
-
 document.querySelectorAll("button").forEach(btn=>{
     btn.addEventListener("click",()=>{
         const value = btn.dataset.val
@@ -19,36 +20,53 @@ document.querySelectorAll("button").forEach(btn=>{
         
         if(value === "="){
             console.log(inputed)
-
             if(op === "power"){
-                var powerx = power(Number(inputed),Number(inputans.value))
+                let powerin = inputarry.splice("^")
+                inputed = powerin[0]
+                inputing = powerin[1]
+                var powerx = power(Number(inputed),Number(inputing))
                 inputans.value = powerx
+                inputarry = [powerx]
                 op = "none"
             }
-            
+
+            else if (op === "squrt"){
+                var squrtx = squrt(inputed)
+                inputans.value = squrtx
+                inputarry = [inputans.value]
+                op = "none"
+            }
+        
             else {
                 try{
                     inputans.value = eval(inputans.value)
-
+                    inputarry = [inputans.value]
                 }catch{
                     inputans.value = "錯誤"
-            }
+                    inputarry = []
+                }
         }
         }
         else if(value === "squrt"){
-            var squrtx = squrt(inputans.value)
-            inputans.value = squrtx
+            inputed = inputans.value
+            inputans.value = inputed + "²"
+            op = "squrt"
         }
         else if(value === "power"){
-            inputed = inputans.value
-            inputans.value = ""
+            inputans.value += "^"
             op = "power"
         }
         else if(value === "AC"){
             inputans.value =""
+            inputarry = []
+        }
+        else if(value === "bs"){
+            inputarry.pop()
+            inputans.value = inputarry.join("")
         }
         else {
-            inputans.value += value
+            inputarry.push(value)
+            inputans.value = inputarry.join("")
         }
     })
 })
